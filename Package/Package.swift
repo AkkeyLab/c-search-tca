2be949f338eb5aaec82c0019663ccc2f66109ed1
AKIO <17640999+AkkeyLab@users.noreply.github.com>
2023-01-08 22:45:49 +0900

    :sparkles: Update dependencies

--- Package/Package.swift
+++ Package/Package.swift
@@ -9,21 +9,19 @@ let package = Package(
         .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Presentation",
             targets: ["Presentation"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.48.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Presentation",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]),
         .testTarget(
             name: "PackageTests",
             dependencies: ["Presentation"]),
