b8434d3008a5149f3f1d1b1d61b4d6b5a1790505
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-02-11 18:23:55 +0900

    :bug: Changed the execution permission of the file

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,6 @@
-# https://developer.apple.com/documentation/xcode/writing-custom-build-scripts
-
 #!/bin/zsh
 
+# https://developer.apple.com/documentation/xcode/writing-custom-build-scripts
 # https://forums.swift.org/t/telling-xcode-14-beta-4-to-trust-build-tool-plugins-programatically/59305/7
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
