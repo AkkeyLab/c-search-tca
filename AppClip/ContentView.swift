166bf77ab17e246241ae7153e5662721280993a4
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-02-12 18:17:44 +0900

    :recycle: Refactor code

--- AppClip/ContentView.swift
+++ AppClip/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ContentViewPreviews: PreviewProvider {
     static var previews: some View {
         ContentView(
             store: Store(
