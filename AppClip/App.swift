badc4dfb84f1997857adeba2b6d2c9065ffabfe9
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-02-12 19:06:54 +0900

    :sparkles: Create App Clip open process

--- AppClip/App.swift
+++ AppClip/App.swift
@@ -5,21 +5,31 @@
 //  Created by AkkeyLab on 2023/02/12.
 //
 
-import ComposableArchitecture
-import Domain
+import Clip
 import SwiftUI
 
 @main
 struct SearchClipApp: App {
+    @State private var companyName: String?
+
     var body: some Scene {
         WindowGroup {
-            ContentView(
-                store: Store(
-                    initialState: SearchCompaniesReducer.State(),
-                    reducer: SearchCompaniesReducer()
-                        .dependency(\.searchCompaniesUseCase, SearchCompaniesUseCase())
-                )
-            )
+            Color(uiColor: .systemBackground)
+                .overlay {
+                    if let companyName {
+                        ClipView(companyName: companyName)
+                    }
+                }
+                .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { userActivity in
+                    guard let incomingURL = userActivity.webpageURL,
+                          let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true),
+                          let queryItems = components.queryItems,
+                          let nameQueryItem = queryItems.first(where: { $0.name == "name" })
+                    else {
+                        return
+                    }
+                    companyName = nameQueryItem.value
+                }
         }
     }
 }
---
+++ Package/Sources/Clip/ClipView.swift
@@ -0,0 +1,54 @@
+//
+//  ClipView.swift
+//  
+//
+//  Created by AkkeyLab on 2023/02/12.
+//
+
+import ComposableArchitecture
+import Domain
+import SwiftUI
+
+public struct ClipView: View {
+    private let store: StoreOf<SearchCompaniesReducer>
+    private let companyName: String
+
+    public init(
+        store: StoreOf<SearchCompaniesReducer> = Store(
+            initialState: SearchCompaniesReducer.State(),
+            reducer: SearchCompaniesReducer()
+                .dependency(\.searchCompaniesUseCase, SearchCompaniesUseCase())
+        ),
+        companyName: String
+    ) {
+        self.store = store
+        self.companyName = companyName
+    }
+
+    public var body: some View {
+        WithViewStore(store) { viewStore in
+            Color(uiColor: .systemBackground)
+                .onAppear {
+                    viewStore.send(.search(companyName: companyName))
+                }
+                .overlay {
+                    if let company = viewStore.companies.first {
+                        Text(company.name)
+                    }
+                }
+        }
+    }
+}
+
+struct ContentViewPreviews: PreviewProvider {
+    static var previews: some View {
+        ClipView(
+            store: Store(
+                initialState: SearchCompaniesReducer.State(),
+                reducer: SearchCompaniesReducer()
+                    .dependency(\.searchCompaniesUseCase, SearchCompaniesUseCase(gateway: SearchCompaniesGatewayMock()))
+            ),
+            companyName: ""
+        )
+    }
+}
