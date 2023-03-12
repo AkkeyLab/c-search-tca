079a16eed2142ffe048d99c1da05dafd2f9e17c6
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-03-12 14:49:27 +0900

    :sparkles: Create widget base file

---
+++ Widget/VisitBundle.swift
@@ -0,0 +1,17 @@
+//
+//  VisitBundle.swift
+//  Visit
+//
+//  Created by AkkeyLab on 2023/03/12.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct VisitBundle: WidgetBundle {
+    var body: some Widget {
+        Visit()
+        VisitLiveActivity()
+    }
+}
