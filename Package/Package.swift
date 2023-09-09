bfd2e5fc8004ac6e3916f6bb01d14516d0dadc11
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-09-10 02:56:11 +0900

    :bug: Fix target version

--- Package/Package.swift
+++ Package/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,8 +7,9 @@ let package = Package(
     name: "Package",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
+        .iOS(.v17),
+        .macOS(.v14),
+        .visionOS(.v1),
     ],
     products: [
         .library(
