8fc64890527942e6510805972fe6217dbd473714
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-12-10 18:00:40 +0900

    :sparkles: Plugin execution on CI was disabled
    Because We deleted SwiftGen

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,5 +2,5 @@
 
 # https://developer.apple.com/documentation/xcode/writing-custom-build-scripts
 # https://forums.swift.org/t/telling-xcode-14-beta-4-to-trust-build-tool-plugins-programatically/59305/7
-defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+# defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 
